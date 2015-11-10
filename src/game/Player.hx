cc4eef69d21b21b204907b1fd143a917a98ae606
MintPaw <jerusanders@gmail.com>
2015-11-10 08:06:49 -0700

    Added gravity to player

--- src/game/Player.hx
+++ src/game/Player.hx
@@ -8,7 +8,16 @@ class Player extends FlxSprite
 	public function new()
 	{
 		super();
+
+		maxVelocity.set(800, 800);
+		acceleration.y = maxVelocity.y * 8;
+		drag.x = maxVelocity.x * 4;
 		
 		makeGraphic(30, 30, 0xFF000055);
+	}
+
+	override public function update(elapsed:Float):Void
+	{
+		super.update(elapsed);
 	}
 }
