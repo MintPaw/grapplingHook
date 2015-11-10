93faeb4d2d56397a04f602852d4261542169f8d7
MintPaw <jerusanders@gmail.com>
2015-11-09 17:18:55 -0700

    Inited Flixel to MainState

---
+++ MainState.hx
@@ -0,0 +1,12 @@
+package ;
+
+import flixel.FlxState;
+
+class MainState extends FlxState
+{
+
+	public function new()
+	{
+		super();
+	}
+}
--- src/Main.hx
+++ src/Main.hx
@@ -1,13 +1,16 @@
 package;
 
 import openfl.display.Sprite;
+import flixel.FlxGame;
 
 class Main extends Sprite
 {
 
-	public function new ()
+	public function new()
 	{
 		super();
 
+		var _flixel:FlxGame = new FlxGame(1280, 720, MainState, 1, 60, 60, true);
+		addChild(_flixel);
 	}
 }
