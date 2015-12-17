a5792956b67fb1137c7dad65e4036134c669729e
MintPaw <jerusanders@gmail.com>
2015-12-17 15:20:15 -0700

    Integrated debugger

--- src/Main.hx
+++ src/Main.hx
@@ -8,6 +8,9 @@ class Main extends Sprite
 
 	public function new()
 	{
+#if debug
+		if (false) new debugger.HaxeRemote(true, "localhost");
+#end
 		super();
 
 		var _flixel:FlxGame = new FlxGame(1280, 720, MainState, 1, 60, 60, true);
